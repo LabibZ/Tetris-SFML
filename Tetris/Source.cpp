7158553f64ef93c8106524603711c1d7aef78fcf
Labib <labib619@hotmail.com>
2024-06-09 22:36:48 -0400

    initial commit; board and pieces set

---
+++ Tetris/Source.cpp
@@ -0,0 +1,58 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+using namespace sf;
+
+const int width = 10;
+const int height = 20;
+
+int field[height][width] = { 0 };
+
+struct Point {
+	int x, y;
+} a[4], b[4];
+
+int figures[7][4] =
+{
+	1,3,5,7, // I
+	2,4,5,7, // Z
+	3,5,4,6, // S
+	3,5,4,7, // T
+	2,3,5,7, // L
+	3,5,7,6, // J
+	2,3,4,5, // O
+};
+
+int main() {
+	RenderWindow window(VideoMode(320, 480), "Tetris");
+	
+	Texture t;
+	t.loadFromFile("include/images/tiles.png");
+
+	Sprite s(t);
+	s.setTextureRect(IntRect(0, 0, 18, 18));
+
+	while (window.isOpen()) 
+	{
+		Event e;
+		while (window.pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+				window.close();
+		}
+
+		int n = 3;
+		for (int i = 0; i < 4; i++) {
+			a[i].x = figures[n][i] % 2;
+			a[i].y = figures[n][i] / 2;
+		}
+
+		window.clear(Color::White);
+		for (int i = 0; i < 4; i++) {
+			s.setPosition(a[i].x * 18, a[i].y * 18);
+			window.draw(s);
+		}
+		window.display();
+	}
+
+	return 0;
+}
