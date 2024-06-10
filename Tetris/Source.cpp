f6a69d5d9b479ec8b875183bcf4a0cfa2f7154c3
Labib <labib619@hotmail.com>
2024-06-09 22:59:31 -0400

    add move, rotate, tick

--- Tetris/Source.cpp
+++ Tetris/Source.cpp
@@ -22,7 +22,47 @@ int figures[7][4] =
 	2,3,4,5, // O
 };
 
-int main() {
+bool check()
+{
+	for (int i = 0; i < 4; i++) {
+		if (a[i].x < 0 || a[i].x >= width || a[i].y >= height) return 0;
+		else if (field[a[i].y][a[i].x]) return 0;
+	}
+	return 1;
+}
+
+void move(int dx) 
+{
+	for (int i = 0; i < 4; i++) {
+		b[i] = a[i];
+		a[i].x += dx;
+	}
+	if (!check()) {
+		for (int i = 0; i < 4; i++) {
+			a[i] = b[i];
+		}
+	}
+}
+
+void rotate() 
+{
+	Point p = a[1]; // center of rotation
+	for (int i = 0; i < 4; i++) {
+		int x = a[i].y - p.y;
+		int y = a[i].x - p.x;
+		a[i].x = p.x - x;
+		a[i].y = p.y + y;
+	}
+	if (!check()) {
+		for (int i = 0; i < 4; i++) {
+			a[i] = b[i];
+		}
+	}
+}
+
+int main() 
+{
+	srand(time(0));
 	RenderWindow window(VideoMode(320, 480), "Tetris");
 	
 	Texture t;
@@ -31,20 +71,56 @@ int main() {
 	Sprite s(t);
 	s.setTextureRect(IntRect(0, 0, 18, 18));
 
+	int dx = 0;
+	bool isRotate = false;
+	int colorNum = 1;
+	float timer = 0.0f;
+	float delay = 0.3f;
+
+	Clock clock;
+
 	while (window.isOpen()) 
 	{
+		float time = clock.getElapsedTime().asSeconds();
+		clock.restart();
+		timer += time;
+
 		Event e;
 		while (window.pollEvent(e))
 		{
 			if (e.type == Event::Closed)
 				window.close();
+			if (e.type == Event::KeyPressed) {
+				if (e.key.code == Keyboard::Up) isRotate = true;
+				else if (e.key.code == Keyboard::Left) dx = -1;
+				else if (e.key.code == Keyboard::Right) dx = 1;
+			}
+		}
+		// move
+		move(dx);
+		// rotate
+		if (isRotate)
+			rotate();
+		// tick
+		if (timer > delay)
+		{
+			for (int i = 0; i < 4; i++) {
+				b[i] = a[i];
+				a[i].y += 1;
+			}
+			timer = 0;
 		}
 
-		int n = 3;
+		int n = 3; // figure
+		if (a[0].x==0)
 		for (int i = 0; i < 4; i++) {
 			a[i].x = figures[n][i] % 2;
 			a[i].y = figures[n][i] / 2;
 		}
+
+		dx = 0;
+		isRotate = false;
+
 
 		window.clear(Color::White);
 		for (int i = 0; i < 4; i++) {
